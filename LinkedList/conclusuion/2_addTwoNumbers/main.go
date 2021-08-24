e1c8064d2ce4593a69f7e2ae04fb4be714868d80
rzimoglyad <romanzimoglyad@digital-spirit.ru>
2021-08-24 15:35:11 +0300

    +addTwoNumbers
    +addTwoNumbers

---
+++ LinkedList/conclusuion/2_addTwoNumbers/main.go
@@ -0,0 +1,196 @@
+package main
+
+import "fmt"
+
+func main() {
+	list := Constructor()
+
+	list.AddAtTail(2)
+	list.AddAtTail(4)
+	list.AddAtTail(3)
+
+	test1 := &ListNode{
+		Val: 5,
+	}
+	test2 := &ListNode{
+		Val: 6,
+	}
+
+	test3 := &ListNode{
+		Val: 4,
+	}
+
+	test1.Next = test2
+	test2.Next = test3
+
+	r := addTwoNumbers(list.head, test1)
+	fmt.Println(r)
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+
+	tail := 0
+	newHead := &ListNode{
+		Val: -1,
+	}
+	head := newHead
+
+	for l1 != nil || l2 != nil {
+		sum := tail
+		if l1 != nil {
+			sum += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			sum += l2.Val
+			l2 = l2.Next
+		}
+		tail = sum / 10
+		sum = sum % 10
+		newHead.Next = &ListNode{
+			Val:  sum,
+			Next: nil,
+		}
+		newHead = newHead.Next
+
+	}
+
+	if tail == 1 {
+		newHead.Next = &ListNode{
+			Val:  1,
+			Next: nil,
+		}
+	}
+	return head.Next
+}
+
+type MyLinkedList struct {
+	length int
+	head   *ListNode
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+/** Initialize your data structure here. */
+func Constructor() MyLinkedList {
+	return MyLinkedList{head: nil}
+}
+
+/** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+func (this *MyLinkedList) Get(index int) int {
+	i := 0
+	if this.head == nil {
+		return -1
+	}
+	if index >= this.length {
+		return -1
+	}
+
+	curNode := this.head
+	for i < index {
+		if curNode.Next == nil {
+			return -1
+		}
+		curNode = curNode.Next
+		i++
+	}
+	return curNode.Val
+}
+
+/** Add a node of value Val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+func (this *MyLinkedList) AddAtHead(val int) {
+	cur := this.head
+
+	newNode := &ListNode{
+		Val:  val,
+		Next: cur,
+	}
+
+	this.head = newNode
+	this.length++
+}
+
+/** Append a node of value Val to the last element of the linked list. */
+func (this *MyLinkedList) AddAtTail(val int) {
+	var curNode *ListNode
+	curNode = this.head
+	newNode := &ListNode{
+		Val: val,
+	}
+	if curNode == nil {
+		this.head = newNode
+		this.length++
+		return
+	}
+	for curNode.Next != nil {
+		curNode = curNode.Next
+	}
+	curNode.Next = newNode
+	this.length++
+}
+
+/** Add a node of value Val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+func (this *MyLinkedList) AddAtIndex(index int, val int) {
+	if index == 0 {
+		this.AddAtHead(val)
+		return
+	}
+	if index == this.length {
+		this.AddAtTail(val)
+		return
+	}
+	if index > this.length {
+		return
+	}
+	var curNode *ListNode
+	curNode = this.head
+	newNode := &ListNode{
+		Val: val,
+	}
+	if curNode == nil {
+		this.head = newNode
+		return
+	}
+	i := 0
+	for i < index-1 {
+		curNode = curNode.Next
+		i++
+	}
+	tmp := curNode.Next
+	newNode.Next = tmp
+	curNode.Next = newNode
+	this.length++
+}
+
+/** Delete the index-th node in the linked list, if the index is valid. */
+func (this *MyLinkedList) DeleteAtIndex(index int) {
+	if index >= this.length {
+		return
+	}
+	if index == 0 {
+		this.head = this.head.Next
+		this.length--
+		return
+	}
+	i := 0
+	curNode := this.head
+	for i < index-1 {
+		curNode = curNode.Next
+		i++
+	}
+	curNode.Next = curNode.Next.Next
+	this.length--
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Get(index);
+ * obj.AddAtHead(Val);
+ * obj.AddAtTail(Val);
+ * obj.AddAtIndex(index,Val);
+ * obj.DeleteAtIndex(index);
+ */
